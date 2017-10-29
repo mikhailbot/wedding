4eb13fa3db9efd89a59b01643e9c938f0d2e88de
mikhaildelport <mikhail@delport.ca>
2017-10-29 16:58:15 -0400

    Add basic header navigation, tachyons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
+    <logo></logo>
+    <navigation></navigation>
     <nuxt/>
   </div>
 </template>
+
+<<script>
+import Logo from '~/components/Logo.vue'
+import Navigation from '~/components/Navigation.vue'
+
+export default {
+  components: {
+    Logo,
+    Navigation
+  }
+}
+</script>
 
 <style>
 html {
@@ -19,34 +33,5 @@ html {
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
