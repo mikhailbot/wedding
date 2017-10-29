172132d0c7756c7f5bc3ce84ceccf8936243e5eb
mikhaildelport <mikhail@delport.ca>
2017-10-29 17:10:36 -0400

    Change logo font, nav styling

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="tc mt4">
-    <div class="satisfy f1 mr4">Amber</div>
-    <div class="satisfy f1 ml4">Mikhail</div>
-    <div class="t6">To be replaced by image with fancy font</div>
+  <div class="tc mt5">
+    <div class="satisfy f1 f-5-ns mr4">Amber</div>
+    <div class="satisfy f1 f-5-ns ml4">Mikhail</div>
   </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Satisfy');
+@font-face {
+    font-family: 'Madina Script';
+    src: url(/fonts/MadinaScriptStd.ttf);
+}
 
 .satisfy {
-  font-family: 'Satisfy', cursive;
+  font-family: 'Madina Script', cursive;
 }
 </style>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="">
-    <nav class="bt bb tc mw7 center mt4">
-      <nuxt-link to="/" class="f6 f5-l link black-80 dib pa3 ph4-l">Home</nuxt-link>
-      <nuxt-link to="" class="f6 f5-l link black-80 dib pa3 ph4-l">RSVP</nuxt-link>
-      <nuxt-link to="" class="f6 f5-l link black-80 dib pa3 ph4-l">Wedding Party</nuxt-link>
-      <nuxt-link to="" class="f6 f5-l link black-80 dib pa3 ph4-l">Venue</nuxt-link>
-      <nuxt-link to="" class="f6 f5-l link black-80 dib pa3 ph4-l">Contact Us</nuxt-link>
+    <nav class="tc mw7 center mt4">
+      <nuxt-link to="/" class="f6 f5-l ttu tracked link black-80 dib pa3 ph4-l">Home</nuxt-link>
+      <nuxt-link to="" class="f6 f5-l ttu tracked link black-80 dib pa3 ph4-l">RSVP</nuxt-link>
+      <nuxt-link to="" class="f6 f5-l ttu tracked link black-80 dib pa3 ph4-l">Wedding Party</nuxt-link>
+      <nuxt-link to="" class="f6 f5-l ttu tracked link black-80 dib pa3 ph4-l">Venue</nuxt-link>
+      <nuxt-link to="" class="f6 f5-l ttu tracked link black-80 dib pa3 ph4-l">Contact Us</nuxt-link>
     </nav>
   </div>
 </template>
